1578c97c56de21572c69e26c54ad817ea95c94d5
osamasabry9 <osamasabry324@gmail.com>
2024-10-05 01:40:02 +0400

    Add SubHeading component and update Header component
    - Import SubHeading component and images constant
    - Update Header component to include SubHeading component and new content
    - Add styling for Header component in Header.css

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React from "react";
 
-import './Header.css';
+import "./Header.css";
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
 
 const Header = () => (
-  <div>
-    Header
+  <div className="app__header app__wrapper section__padding" id="home">
+    <div className="app__wrapper_info">
+      <SubHeading title="Chase the new flavour" />
+      <h1 className="app__header-h1">The Key To Fine Dining</h1>
+      <p className="p__opensans" style={{ margin: "2rem 0" }}>
+        Sit tellus what you want, where you want, when you want. Our restaurant
+        will treat you like family.
+      </p>
+      <button type="button" className="custom__button">
+        Explore Menu
+      </button>
+    </div>
+    <div className="app__wrapper_img">
+      <img src={images.welcome} alt="header_img" />
+    </div>
   </div>
 );
 
