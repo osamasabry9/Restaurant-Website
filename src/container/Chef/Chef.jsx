1b6307f655a5655a17f03a01dff8edb446fe05a5
osamasabry9 <osamasabry324@gmail.com>
2024-10-05 14:45:30 +0400

    Add responsive styling for Chef section
    Update Chef component CSS to include responsive styling for the chef section
    to ensure proper display on different screen sizes. Also, import necessary
    components and constants in Chef.jsx file for better organization and
    readability.

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
+import React from "react";
 
-import './Chef.css';
+import { SubHeading } from "../../components";
+import { images } from "../../constants";
+import "./Chef.css";
 
 const Chef = () => (
-  <div>
-    Chef
+  <div className="app__bg app__wrapper section__padding" id="chef">
+    <div className="app__wrapper_img app__wrapper_img-reverse">
+      <img src={images.chef} alt="chef_img" />
+    </div>
+
+    <div className="app__wrapper_info">
+      <SubHeading title="Chef's Word" />
+      <h1 className="headtext__cormorant">What We Believe In</h1>
+      <div className="app__chef-content">
+        <div className="app__chef-content_quote">
+          <img src={images.quote} alt="quote_img" />
+          <p className="p__opensans">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+          </p>
+        </div>
+        <p className="p__opensans">
+          Eaque voluptatem, voluptates doloremque, voluptatibus, voluptate sit
+          quasi quidem quae quos. Lorem ipsum dolor sit amet consectetur
+          adipisicing elit. Eaque voluptatem, voluptates doloremque,
+          voluptatibus, voluptate sit quasi quidem quae quos.
+        </p>
+      </div>
+
+      <div className="app__chef-sign">
+        <p>Kevin Luo</p>
+        <p className="p__opensans">Chef & Founder</p>
+        <img src={images.sign} alt="sign_img" />
+      </div>
+    </div>
   </div>
 );
 
